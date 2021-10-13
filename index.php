4dab9b6c98b8b2b04ce6acf32b14de7ac2886d1b
Gabriel Boeira <52590011+GabrielMBoeira@users.noreply.github.com>
2021-10-13 09:22:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -959,7 +959,7 @@
         <!-- You can delete the links only if you purchased the pro version. -->
         <!-- Licensing information: https://bootstrapmade.com/license/ -->
         <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ -->
-        Desenvolvido por <a href="https://bootstrapmade.com/">GoFood Token</a>
+        Desenvolvido por <a href="#">GoFood Token</a>
       </div>
     </div>
   </footer><!-- End Footer -->
